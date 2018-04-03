00c0ff090392abc3bd8ded40427aa451aaa4b29a
Ritwik Awasthi <awasthi.ritwik@gmail.com>
2018-04-03 14:14:30 -0600

    SQL script for images and leader board database
    creates database, uses it, makes images, populates images, makes leader board, DOES NOT populate leader board yet

---
+++ finksdb.sql
@@ -0,0 +1,23 @@
+create database if not exists finksTest;
+use finksTest;
+
+create table if not exists images(
+	title varchar(40),
+	votes int(10),
+	url varchar(2048) not null
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+insert into images (title, votes, url) values
+('image1', 2, "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350"),
+('image2', 10, "https://wallpaperbrowse.com/media/images/soap-bubble-1958650_960_720.jpg"),
+('image3', 1, "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"),
+('image4', 30, "https://wallpaperbrowse.com/media/images/cat-1285634_960_720.png"),
+('image5', 4, "https://media.istockphoto.com/photos/plant-growing-picture-id510222832?k=6&m=510222832&s=612x612&w=0&h=Pzjkj2hf9IZiLAiXcgVE1FbCNFVmKzhdcT98dcHSdSk=");
+
+create table if not exists leaderboard(
+id int(1) not null auto_increment,
+url varchar(2048) not null,
+primary key (id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+
