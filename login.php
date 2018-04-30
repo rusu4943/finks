80e6bb66f4f617eb7c2809b50272e24fee82a2a7
ZormeneKinanee <33584010+ZormeneKinanee@users.noreply.github.com>
2018-04-30 08:00:45 -0600

    Update login.php
    updated

--- login.php
+++ login.php
@@ -1,24 +1,22 @@
 <?php
-	public function login($POST)
-	{
-		$username = trim($_POST['login__username']);
-		$password = trim($_POST['login__password']);
+include_once('db.php');
+$db = new db();
+if($_POST){
+	$username = trim($_POST['name']);
+	$password = trim($_POST['pwd']);
 
-		$dataArr = $db->fetch_one("select * from users where username = '$username' and password = '$password' ");
-		if(!$dataArr){
-			echo "<script>alert('ERROR Incorrect username or password!');history.back();</script>";
-			return 1;
-		}
+	$dataArr = $db->fetch_one("select * from users where username = '$username' and password = '$password' ");
+	if(!$dataArr){
+		echo "<script>alert('ERROR Incorrect username or password!');history.back();</script>";
+		exit;
+	}
 
-		setcookie("blog_uid",$dataArr['uid'], time()+3600*24);
-		echo "<script>alert('Login success.');location.href='search.php';</script>";
-		return 0;
-	}
-	include_once('db.php');
-	$db = new db();
-	if($_POST){
-		login($_POST);
-	}
+	setcookie("blog_uid",$dataArr['uid'], time()+3600*24);
+	echo "<script>alert('Login success.');location.href='Homepagesearch.php';</script>";
 	exit;
+}
+	echo "<script>location.href='Login.html';</script>";
+
+exit;
 ?>
 
